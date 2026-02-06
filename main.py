7209d94371f87a51e3d07799fd35b82c41b0b272
Your Name <you@example.com>
2026-02-06 18:33:40 +0530

    data validation completed

---
+++ main.py
@@ -0,0 +1,27 @@
+from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\n========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>>> {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.initiate_data_validation()
+    logger.info(f">>>>>>> {STAGE_NAME} completed <<<<<<<\n\n====")
+except Exception as e:
+    logger.exception(e)
+    raise e
